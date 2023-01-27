e07073f505580dfe0ba8cbab982045865e3b7bcb
rgil-splunk <70550165+rgil-splunk@users.noreply.github.com>
2023-01-26 16:16:38 -0800

    Add slackbot connector (#1)
    * Initial commit
    
    * Remove initial readme.html
    
    * Update app json
    
    * Run through pre-commit
    
    * Fix file copyright headers
    
    * Replace ERR with ERROR
    
    * Fix inconsistent variable names
    
    * More variable name fixes
    
    * Replace CONN with CONNECTION
    
    * Code cleanup and refactoring
    
    * Action fixes
    
    * Fix inconsistent quotes
    
    * Sort list containers output
    
    * Refactor slackbot endpoint calls
    
    * Remove six dependency
    
    * Update date
    
    * Refactor into separate commands
    
    * Remove unused queue code
    
    * Minor bug fixes
    
    * Move parsing and auth logic to each command
    
    * Add new get_container filters
    
    * Remove unused token from config
    
    * Remove unused token
    
    * Rename SOAR token
    
    * Fix get_container bugs
    
    * Add short flag for get_container
    
    * Split up list command
    
    * Fix playbook status filter
    
    * Add debug command
    
    * Fix argparse fail log logic
    
    * Fix run_playbook name bug
    
    * Refactor run action command
    
    * Add release notes
    
    * Move enums to separate file
    
    * Prefix all files with sb
    
    * Fix configuration order
    
    * Fix copyright
    
    * Update requirements
    
    * Fix copyright
    
    * Add fallback for NRI port
    
    * Update README.md
    
    * Remove unused timeout config
    
    * Update screenshots
    
    * Update docs with config info
    
    * Remove false statement about stopping ingestion
    
    * Bug fixes
    
    * Add troubleshooting section to docs
    
    Co-authored-by: splunk-soar-connectors-admin <admin@splunksoar>

---
+++ commands/sb_command.py
@@ -0,0 +1,61 @@
+# File: sb_command.py
+#
+# Copyright (c) 2023 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+import logging
+
+import slack_bot_consts as constants
+from slack_bot_enums import CommandPermission
+
+
+class Command:
+    """ Slack Bot command base class. """
+
+    COMMAND_NAME: str = None
+    COMMAND_DESCRIPTION: str = None
+
+    def __init__(self, slack_bot, channel):
+        self.slack_bot = slack_bot
+        self.channel = channel
+
+        if self.COMMAND_NAME is None:
+            raise ValueError(f'COMMAND_NAME must be specified for the command {self.__class__.__name__}.')
+
+    def configure_parser(self, parser) -> None:
+        """ Configure the parser for this command. """
+        raise NotImplementedError
+
+    def check_authorization_and_execute(self, parsed_args) -> str:
+        """
+        Check authorization and execute the command.
+
+        Return a message with the result.
+        """
+        if not self.check_authorization():
+            return constants.SLACK_BOT_ERROR_COMMAND_NOT_PERMITTED
+
+        return self.execute(parsed_args)
+
+    def check_authorization(self) -> bool:
+        """ Return True if authorized to run command. """
+        expected_permission = CommandPermission[self.COMMAND_NAME.upper()]
+        if self.slack_bot.command_permissions[expected_permission]:
+            logging.debug('**Command: "%s" is permitted', self.COMMAND_NAME)
+            return True
+
+        logging.debug('**Command: "%s" is not permitted', self.COMMAND_NAME)
+        return False
+
+    def execute(self, parsed_args) -> str:
+        """ Execute the specified  the command using the specified arguments and return a message with the result. """
+        raise NotImplementedError
